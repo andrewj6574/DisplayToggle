2c46fb120d14dd48a2084feda4c19675e7178ad4
Andrew Jackson <andrewj6574@gmail.com>
2023-03-11 10:44:52 -0800

    open and close sound dialog to fix audio not automatically being detected

--- DisplaySwitcher.cpp
+++ DisplaySwitcher.cpp
@@ -6,6 +6,47 @@
 #include <Windows.h>
 #include <vector>
 
+void StartSoundProcess()
+{
+   // additional information
+   STARTUPINFOA si = {0};
+   PROCESS_INFORMATION pi = {0};
+
+   // set the size of the structures
+   si.cb = sizeof(si);
+   //si.dwFlags = STARTF_USESHOWWINDOW;
+   //si.wShowWindow = SW_HIDE;
+   //si.wShowWindow = FALSE;
+   char command_line[] = R"(C:\Windows\System32\cmd.exe /K control mmsys.cpl)";
+
+   // start the program up
+   auto ret = CreateProcessA
+   (
+      NULL,      // the path
+      command_line,                // Command line
+      NULL,                   // Process handle not inheritable
+      NULL,                   // Thread handle not inheritable
+      FALSE,                  // Set handle inheritance to FALSE
+      CREATE_NO_WINDOW,     // Opens file in a separate console
+      NULL,           // Use parent's environment block
+      NULL,           // Use parent's starting directory 
+      &si,            // Pointer to STARTUPINFO structure
+      &pi           // Pointer to PROCESS_INFORMATION structure
+   );
+
+   // wait for sound window
+   HWND	SoundsDialogHandle;
+   while ((SoundsDialogHandle = FindWindow(L"#32770", L"Sound")) == NULL);
+
+   // clean up and close sounds dialog window
+   PostMessage(SoundsDialogHandle, WM_CLOSE, NULL, 0);
+
+   TerminateProcess(pi.hProcess, 0);
+
+   CloseHandle(pi.hThread);
+   CloseHandle(pi.hProcess);
+}
+
 void ToggleDisplay(int monitor)
 {
    DWORD           DispNum = 0;
@@ -37,7 +78,7 @@ void ToggleDisplay(int monitor)
             DEVMODE    DevMode;
             ZeroMemory(&DevMode, sizeof(DevMode));
             DevMode.dmSize = sizeof(DevMode);
-            DevMode.dmFields = DM_PELSWIDTH | DM_PELSHEIGHT | DM_POSITION | DM_DISPLAYFREQUENCY | DM_DISPLAYFLAGS;
+            DevMode.dmFields = DM_PELSWIDTH | DM_PELSHEIGHT | DM_POSITION;
             ChangeDisplaySettingsEx(DisplayDevice.DeviceName, &DevMode, NULL, CDS_UPDATEREGISTRY, NULL);
             }
          else
@@ -51,6 +92,9 @@ void ToggleDisplay(int monitor)
             //DevMode.dmPelsHeight = 2160;
             ChangeDisplaySettingsEx((LPCWSTR)DisplayDevice.DeviceName, &DevMode, NULL, CDS_UPDATEREGISTRY | CDS_NORESET, NULL);
             ChangeDisplaySettingsEx(NULL, NULL, NULL, 0, NULL);
+
+            //int result = system("control mmsys.cpl,,0");
+            StartSoundProcess();
          }
       }
 
@@ -69,6 +113,8 @@ int main()
    AllocConsole();
    hWnd = FindWindowA("ConsoleWindowClass", NULL);
    ShowWindow(hWnd, 0);
+
+   //StartProcess("control mmsys.cpl");
 
    ToggleDisplay(3);
 
